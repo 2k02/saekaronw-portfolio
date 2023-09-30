68e12d37ec794a3508398c0ae82afed58e5bbf20
Saekaronwt <saekaronw@gmail.com>
2023-09-30 14:46:17 -0500

    change adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config'
-
-import node from '@astrojs/node'
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: netlify(),
 })
